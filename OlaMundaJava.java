486f8dee7cb27675a674d40442d87674a2962462
MichelSoares <michel_soares@outlook.com>
2015-06-26 15:59:48 -0300

    criando metodo de saudacao no JAVA

--- OlaMundaJava.java
+++ OlaMundaJava.java
@@ -2,7 +2,15 @@ public class OlaMundo{
 
 	public static void main(String []args){
 		
-		System.out.println("Olá mundo com JAVA!!!");
+		
+		saudacao("Olá mundo com JAVA!!!");
+		
+	}
+	
+	public void saudacao(String mensagem){
+	
+		System.out.println(mensagem);
+	
 	}
 
 }
